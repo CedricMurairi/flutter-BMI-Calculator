3a6486806fcd0a620f6ee8a444d4e7f5f3137fb8
Cedric Murairi <murairicedric@gmail.com>
2020-05-04 21:30:39 +0200

    Initializing my flutter BMI calculator

---
+++ lib/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const bottomBarHeight = 80;
+const bottomBarColor = Color(0xFFEB1555);
+const activeColor = Color(0xFF1D1E33);
+const inactiveColor = Color(0xFF111328);
+
+const styleText = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+  color: Color(0xFF8D8E98),
+);
+
+const numberStyle = TextStyle(
+  fontWeight: FontWeight.w900,
+  fontSize: 50,
+);
+
+enum Gender {
+  male,
+  female,
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Home_page(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        accentColor: Colors.blueAccent,
+      ),
+    );
+  }
+}
